74236b4bf80a1dc88f37a85c71967bfa7e55cc0c
Ovinu Madeesha <ovinumadeesha23@gmail.com>
2025-06-13 23:21:13 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './componets/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+   <div>
+
+
+    <LoginSignup/>
+
+
+    </div>
+  );
+}
+
+export default App;
